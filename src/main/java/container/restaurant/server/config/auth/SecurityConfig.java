89099fd00c2f71da53a7a7f581d4937c7d4a1442
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-08-07 10:04:59 +0900

    :construction: temp

--- src/main/java/container/restaurant/server/config/auth/SecurityConfig.java
+++ src/main/java/container/restaurant/server/config/auth/SecurityConfig.java
@@ -1,10 +1,17 @@
 package container.restaurant.server.config.auth;
 
+import container.restaurant.server.utils.jwt.JwtLoginService;
+import container.restaurant.server.utils.jwt.jjwt.JjwtLoginService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpMethod;
+import org.apache.http.HttpStatus;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -14,6 +21,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .cors().and().csrf().disable()
+//                .sessionManagement(s -> s
+//                        .sessionCreationPolicy(SessionCreationPolicy.NEVER))
+//                .exceptionHandling(e -> e
+//                        .authenticationEntryPoint((request, response, ex) ->
+//                                response.sendError(HttpStatus.SC_UNAUTHORIZED, ex.getMessage())))
+//                .authorizeRequests(a -> a
+//                        .antMatchers(GET).permitAll()
+//                        .antMatchers(POST, "/api/user/login", "/api/user").permitAll()
+//                        .anyRequest().authenticated());
+
+
         http
                 .csrf().disable()
                 .headers(h -> h
